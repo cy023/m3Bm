a49d0630884946dc33b4fa6d2e0634d04b7d9eb3
cyyang <cyyang@g.ncu.edu.tw>
2021-06-16 20:33:47 +0800

    Test user led function

--- src/main.c
+++ src/main.c
@@ -1,10 +1,20 @@
-#include "STM32F107xC.h"
+#include "core_cm3.h"
+#include "stm32f107xc.h"
+#include "gpio.h"
 
-uint32_t global_init_var = 1111;
+uint32_t global_init_var = 6666;
 uint32_t global_unin_var;
 
 int main()
 {
     int local_var;
+    gpio_init();
+    gpio_off();
+    while (1) {
+        gpio_off();
+        delay_();
+        gpio_on();
+        delay_();
+    }
     return 0;
 }
